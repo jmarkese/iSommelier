f5cc9a5dab94f119c443454d43bb9fe810104bde
sunithav82 <36507442+sunithav82@users.noreply.github.com>
2018-04-02 11:58:59 -0500

    Update wineimportwithFK.py

--- dataimport/wineimportwithFK.py
+++ dataimport/wineimportwithFK.py
@@ -18,16 +18,19 @@ cursor.execute('SET character_set_connection=utf8;')
 with open('Wine.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        cursor.execute("SELECT id FROM winereviews_winery WHERE concat(name)=%s;", [row[4]])
-        winery_id = cursor.fetchone()
-        if winery_id is None:
+        try:   
+            cursor.execute("SELECT id FROM winereviews_winery WHERE concat(name)=%s;", [row[4]])
+            winery_id = cursor.fetchone()
+            if winery_id is None:
             winery_id = 1;
-        cursor.execute("SELECT id FROM winereviews_variety WHERE concat(name)=%s;", [row[5]])
-        variety_id = cursor.fetchone()
-        if variety_id is None:
-            variety_id = 1;
-        values = [row[0],row[1],row[2],row[3], variety_id, winery_id, row[5]]
-        cursor.execute('INSERT INTO winereviews_wine(id, name, price, description, variety_id , winery_id, varietal, created_at, updated_at ) VALUES(%s,%s,%s,%s,%s,%s,%s, NOW(),NOW())', values)
-
-mydb.commit()
+            cursor.execute("SELECT id FROM winereviews_variety WHERE concat(name)=%s;", [row[5]])
+            variety_id = cursor.fetchone()
+            if variety_id is None:
+                variety_id = 1;
+            values = [row[0],row[1],row[2],row[3], variety_id, winery_id, row[5]]
+            cursor.execute('INSERT INTO winereviews_wine(id, name, price, description, variety_id , winery_id, varietal, created_at, updated_at ) VALmydb.commit()UES(%s,%s,%s,%s,%s,%s,%s, NOW(),NOW())', values)
+            if row[0] % 1000 == 0 || int(row[0]) == 129970:
+                mydb.commit()
+        except:
+            print (row[5])
 cursor.close()
