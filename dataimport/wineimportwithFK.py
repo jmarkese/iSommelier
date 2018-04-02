e25ce38f1506f6d150263e990e6c9c7ebdd9748f
sunithav82 <36507442+sunithav82@users.noreply.github.com>
2018-04-01 22:10:59 -0500

    Update wineimportwithFK.py

--- dataimport/wineimportwithFK.py
+++ dataimport/wineimportwithFK.py
@@ -22,7 +22,7 @@ with open('wine.csv', 'r') as csvfile:
         winery_id = cursor.fetchone()
         cursor.execute("select id from variety where name =%s;" [row[6]])
         variety_id = cursor.fetchone()
-        values = [row[1],row[2],row[3],row[4], winery_id, variety_id, row[6]]
+        values = [row[1],row[2],row[3],row[4], variety_id, winery_id, row[6]]
         cursor.execute('INSERT INTO winereviews_wine(id, name, price, description, variety_id , winery_id, varietal, created_at, updated_at ) VALUES(%s,%s,%s,%s,%s,%s,%s, NOW(),NOW())', values)
 
 mydb.commit()
