b3b534f735a736433294903859d25ca8ed7b80aa
John Markese <john.markese@gmail.com>
2018-04-05 14:57:44 +0000

    added D3 endpoints, created template structure, static js and images

--- isommelier/isommelier/settings.py
+++ isommelier/isommelier/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'isommelier.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,5 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
